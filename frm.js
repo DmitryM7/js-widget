3e522f7a4d9227935fc763ca3fceddea032b7366
Maslov Dmitry <dmitrii_m@inbox.ru>
2019-11-27 22:30:58 +0300

    Поднял актуальный файл + изменения по применению изменений по Ctrl + Enter.

--- frm.js
+++ frm.js
@@ -9,39 +9,43 @@
 (function ($, undefined) {
   $.widget("o.frm",{
  options: {
-            id           : null,
-            url          : null,
-            frmObject    : null,
-            params       : {},
-            saveAction   : 'save',
-            loadAction   : 'load',
-            buttons      : ['apply','save'],
-            width        : '950px',
-            _title       :null
+     url: null,
+     frmObject: null,
+     params: {},
+     _paramsSended: {},
+     saveAction: 'save',
+     applyAction: 'apply',
+     okAction: 'ok',
+     loadAction: 'load',
+     showButtons: ['apply', 'save'],
+     buttons: [],
+     width: '950px',
+     useCtrlEnter:true,
+     _title: null,
+     showTitle: true
  },
-setParam: function(paramName,paramValue) {
+      close : function () {
+          var self=this;
+          self.options.frmObject.dialog('close');
+          self._trigger('_afterclose',{},{parent:self});
+      },
+      setParam: function(paramName,paramValue) {
   this.options.params[paramName]=paramValue;
 },
-/** Открыть **/
+      /** Открыть **/
 open : function () {
-var self=this;
-         if (self._trigger('_beforeopen',{},{parent:self,action:self.options._currAction})===false) {
-                                               return;
-         };
+
           this._fillLocal();
           this._fillRemote();
-          /*
+          /****************************************
            * Если id не указан, то данные
            * по средствам AJAX мы не получим.
-           */
-          if (this.options.id==null) {
+           ***************************************/
+
+          if (this.options.id==null && this._beforeOpen()!==false) {
               this.options.frmObject.dialog('open');
           };
 
-},
-close : function () {
-    var self=this;
-    self.options.frmObject.dialog('close');
 },
 /** Установить искомый ID **/
 setId   : function (id)  {
@@ -70,6 +74,15 @@ getParams : function () {
 getParam  : function (param) {
   return this.options.params[param];
 },
+getParamSended : function (param) {
+    var self=this;
+
+    if (self.options._paramsSended.hasOwnProperty(param)) {
+        return self.options._paramsSended[param];
+    };
+    return null;
+},
+
 /** Полная очистка форма + пар-ры **/
 clear: function () {
   this._clear();
@@ -77,6 +90,8 @@ clear: function () {
 },
 /** Отобразить **/
 _create : function (conf) {
+
+
     $.extend(this.options,conf);
     this.options.frmObject = this.element;
     this._createDialog();
@@ -89,31 +104,57 @@ _createDialog : function () {
           var self=this;
           var buttons=[];
 
-          if ($.inArray('apply',self.options.buttons)!=-1) {
+          $.extend(buttons,self.options.buttons);
 
+          self.options.pid = 'frm-' + $(self.element).attr('id');
+
+          if ($.inArray('apply',self.options.showButtons)>-1) {
               buttons.push({
                          text:'Применить',
                          click:function () {
-                                           self.options._currAction='apply';
-                                           if (self._trigger('_onclick',{},{parent:self,action:self.options._currAction})===false) {
+                                           self.options._currAction=self.options.applyAction;
+                                           self._trigger('_onclick',{},{parent:self,action:self.options._currAction});
+
+                                           if (self._beforeSave()===false) {
                                                return;
                                            };
                                            self._saveEdit();
-                         }
+                         },
+                  'class':'btn'
               });
           };
 
-          if ($.inArray('save',self.options.buttons)!=-1) {
-
+          if ($.inArray('save',self.options.showButtons)>-1) {
               buttons.push({
-                  text:'Сохранить',
+                      text:'Сохранить',
                       click:function () {
+						
                                          self.options._currAction=self.options.saveAction;
-                                         if (self._trigger('_onclick',{},{parent:self,action:self.options._currAction})===false) {
+                                         self._trigger('_onclick',{},{parent:self,action:self.options._currAction});
+
+                                        if (self._beforeSave()===false) {
                                             return;
-                                         };
+                                        };
                                          self._saveEdit();
-                                        }
+                                        },
+                      'class':'btn'
+              });
+          };
+
+          if ($.inArray('ok',self.options.showButtons)>-1) {
+              buttons.push({
+                  text:'ОК',
+                  click:function () {
+
+                      self.options._currAction=self.options.okAction;
+                      self._trigger('_onclick',{},{parent:self,action:self.options._currAction});
+
+                      if (self._beforeSave()===false) {
+                          return;
+                      };
+                      self._saveEdit();
+                  },
+                  'class':'btn'
               });
           };
 
@@ -123,7 +164,8 @@ _createDialog : function () {
                    self.options._currAction='cancel';
                    self._trigger('_onclick',{},{parent:self,action:self.options._currAction});
                    self._clear();
-               }
+               },
+              'class':'btn'
            });
 
           self._adorn();
@@ -133,12 +175,31 @@ _createDialog : function () {
               autoOpen:false,
               modal:true,
               buttons:buttons,
+              dialogClass: self.options.pid,
+              closeText: "Закрыть",
               beforeClose : function (event,ui) {
                   self._trigger('_beforeclose',{},{parent:self,event:event,ui:ui,action:self.options._currAction});
               }
           });
           // Запоминаем первоначальный заголовок
           this.options._title=this.options.frmObject.dialog('option','title');
+
+          var closeButton=$('<div style="position:absolute;right:18px;top:17px"></div>').
+                        append($('<a href="#"><img src="../../../images/verstka/close.jpg"></a>').click(function (e) {
+                            self.close();
+                            return false;
+                        }));
+
+          if  (!self.options.showTitle) {
+        $('.' + self.options.pid).find('.ui-dialog-titlebar').remove();
+        $('.' + self.options.pid).find('.ui-tabs-nav').append(closeButton);
+    } else {
+        $('.' + self.options.pid).find('.ui-dialog-title').append(closeButton);
+        $('.' + self.options.pid).find('.ui-dialog-titlebar-close').remove();
+    };
+
+          self._bindKeyEvents();
+
       },
 
 /**
@@ -153,6 +214,15 @@ _createDialog : function () {
             processData = !hasFileReader,
             contentType = hasFileReader ? false : 'application/x-www-form-urlencoded; charset=UTF-8';
 
+
+    /**
+     * Если не прописан внешний URL, то считаем, что сохранение прошло.
+     */
+    if (self.options.url==null) {
+        self._afterSave();
+        return;
+    };
+
           // Если есть считыватель файлов, то adorn вернет
           // FormData иначе {}
 
@@ -161,17 +231,14 @@ _createDialog : function () {
             params = new FormData();
             params=self.element.adorn('get',params);
 
-	    if (self.options.id) {
-	        params.append('id',self.options.id);
-	    };
-          $.each(self.options.params,function (key,value) {
-                params.append(key,value);
-            });
+            $.each(self.options.params,function (key,value) {
+                        params.append(key,value);
+             });
          } else {
                 params=self.element.adorn('get');
-                params['id']=self.options.id;
                 $.extend(params,self.options.params);
          };  
+
 
     $.ajax({
         url: url,
@@ -182,6 +249,7 @@ _createDialog : function () {
         contentType: contentType,
         dataType:'json',
         success: function (res) {
+            $.extend(self.options._paramsSended,self.element.adorn('get'));
             self._afterSave(res);
         }
     });
@@ -194,9 +262,10 @@ _clear: function () {
  },
 /** Загрузить с сервиса **/
 _fillRemote : function () {
-            var self= this;
-            var m   = {};
-            var currElement, currElementModel;
+            var self= this,
+                m   = {},
+                currElement,
+                currElementModel;
 
             $.extend(self.options.params,{id:self.options.id});
 
@@ -205,6 +274,9 @@ _fillRemote : function () {
                 this._doAction(self.options.loadAction,self.options.params,function (result) {
 
                     self.element.adorn('fillElements',result);
+
+                    self._trigger('_afterload',{},{parent:self,result:result});
+
                     /**
                      * Если айдишник указан, то данные мы обязаны получить.
                      */
@@ -232,10 +304,10 @@ _fillLocal  : function () {
          * @private
          */
 _adorn   : function () {
-            var self=this;
-            self._bindToEvents();
-            self.element.adorn();
-        },
+    var self=this;
+    self._bindToEvents();
+    self.element.adorn();
+},
 
 _doAction     : function (name,params,onEndAction) {
             var self=this;
@@ -244,7 +316,7 @@ _doAction     : function (name,params,onEndAction) {
             });
         },
 _makeUrl      : function (action) {
-            return this.options.url + (action != null ? '/' + action : "");
+            return this.options.url+(action!=null ? '/'+action : "");
         },
 _bindToEvents: function () {
             var self=this;
@@ -253,7 +325,7 @@ _bindToEvents: function () {
                     self._afterSave(queueData);
                 });
             self.element.bind('adorn_onclear',function (e,rrr) {
-                    self.options.frmObject.dialog('close');
+                    self.close();
             });
             self.element.bind('adorn_onready',function(e,finfo) {
 
@@ -282,10 +354,18 @@ _afterSave : function (res) {
           self._trigger('_aftersave',{},{parent:self,result:res});
           self.close();
       },
+_beforeOpen: function () {
+    var self=this;
+    return self._trigger('_beforeopen',{},{parent:self});
+ },
+_beforeSave: function () {
+    var self = this;
+    return self._trigger('_beforesave',{},{parent:self,action:self.options._currAction});
+},
 _afterCancel : function () {
           var self=this;
           self._trigger('_aftersave',{},{parent:self});
-          self.options.frmObject.dialog('close');
+          self.close();
       },
  setTitle: function (title) {
     this.options.frmObject.dialog('option','title',title);
@@ -295,8 +375,86 @@ _afterCancel : function () {
  },
  getTitle:function () {
      return this.options.frmObject.dialog('option','title');
- }
+ },
+_bindKeyEvents: function () {
+    var self=this;
+
+
+
+    if (self.options.useCtrlEnter) {
+        $('.'+self.options.pid).on('keyup',function (e) {
+
+            if ((e.ctrlKey || e.metaKey) && (e.keyCode == 13 || e.keyCode == 10)) {
+
+                self.options._currAction=self.options.okAction;
+                self._trigger('_onclick',{},{parent:self,action:self.options._currAction});
+
+                if (self._beforeSave()===false) {
+                    return;
+                };
+
+                self._saveEdit();
+            };
+        });
+    };
+
+}
 });
 })( jQuery );
 
+/*
+jQuery(function($) {
+//alert($('.footer').css('position'));
+ PopUpHide();
+  // здесь код скрипта, где в $ будет находиться объект, предоставляющий доступ к функциям jQuery
+/*
+$(".wibor_ustr_list").hover(function() {
+$(".wiborite_ustr").removeClass('opa');
+});
 
+$(".wiborite_ustr").hover(function() {
+$(".wiborite_ustr").removeClass('opa');
+});*/
+/*$(".wibor_ustr_pic").click(function() {
+
+
+});
+
+/*$(document).click(function() {
+
+//$( this ).removeClass( "hover" );
+//$( ".wiborite_ustr" ).mouseleave();
+//$( ".wibor_ustr_list" ).mouseleave();
+/*
+if (jQuery(this).attr('class') == 'wibor_ustr_pic') {
+alert(jQuery(this).attr('class'));
+$(".wiborite_ustr").css("opacity",'1');
+$(".wiborite_ustr").css("left",'0');
+} else {
+alert(jQuery(this).attr('class'));
+$(".wiborite_ustr").css("opacity",'0');
+$(".wiborite_ustr").css("left",'-99999px');
+}
+*/
+//$(".wiborite_ustr").css("left",'-99999999999px');
+
+//$(".wiborite_ustr").addClass("opa");
+//$(".wiborite_ustr").removeClass('opa');
+/*
+});
+
+$( ".dob_ustr" ).click(function() {
+
+    $("#popup1").show();
+});
+
+})*/
+/*
+function PopUpShow(){
+
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+;*/
